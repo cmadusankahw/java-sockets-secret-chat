727847c6e27177188179d8462452f5452ea019d7
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-06-23 11:55:09 +0530

    removing unnecessary codes

--- ChatServer/src/com/secret/chat/serverMain.java
+++ ChatServer/src/com/secret/chat/serverMain.java
@@ -3,7 +3,7 @@ package com.secret.chat;
 
 public class serverMain {
     public static void main(String[] args) {
-        int port = 8188;
+        int port = 7800;
         Server server = new Server(port);
 
         server.start();
